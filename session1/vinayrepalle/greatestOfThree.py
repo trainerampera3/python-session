37545d606557a5f1997301ce3b315013bd2f5f30
Vinay-Ampera <Vinay.repalle@amperatech.ai>
2026-08-19 18:12:29 +0530

    intial commit

---
+++ session1/vinayrepalle/greatestOfThree.py
@@ -0,0 +1,12 @@
+# gratest in 3 numbers 
+a = int(input("Enter number a = "))
+b = int(input("Enter number b = "))
+c = int(input("Enter number c = "))
+if(a>b):
+    if(a>c):
+        print(a)
+else:
+    if(b>c):
+        print(b)
+    else:
+        print(c)
