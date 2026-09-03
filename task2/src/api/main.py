36663177dcee6a0a0267aab8928ae675bf7f85b2
GajapathiRaoAmpera <gajapathi.mugunda@amperatech.ai>
2026-09-03 16:44:20 +0530

    Add customers_group endpoint for managing customer groups

--- task2/src/api/main.py
+++ task2/src/api/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from api.routes import customers
+from api.routes import customers , customers_group
 
 def create_app() -> FastAPI:
     app = FastAPI()
 
     app.include_router(customers.router)    
+    app.include_router(customers_group.router)
 
     @app.get("/")
     def read_root():
