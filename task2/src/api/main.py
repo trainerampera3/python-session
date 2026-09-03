8661360ca6b5f87fc19810c7ca636f4269324121
GajapathiRaoAmpera <gajapathi.mugunda@amperatech.ai>
2026-09-03 17:20:20 +0530

    Modified for the ease of access

--- task2/src/api/main.py
+++ task2/src/api/main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 from api.routes import customers , customers_group , customer_address
+#Vinay
+#sahir
+#Jay
+#Jhansi
+#Deepika
+#Pushpa
 
 def create_app() -> FastAPI:
     app = FastAPI()
@@ -7,6 +13,27 @@ def create_app() -> FastAPI:
     app.include_router(customers.router)
     app.include_router(customers_group.router)
     app.include_router(customer_address.router)
+    
+    #Orders-Sahir
+    
+    
+    
+    
+
+    #Orders2-Jayanth
+    
+    
+    
+    
+    #Orders3-Pushpa
+    
+    
+    #products-Vinay
+    
+    
+    
+    
+    #Stores-Deepika
 
     @app.get("/")
     def read_root():
