282eb2ea61227247c6d570fe76fd4d71041b0903
deepikaampera <deepika.katam@amperatech.ai>
2026-09-03 17:38:58 +0530

    store endpoints are created

--- task2/src/database/connection.py
+++ task2/src/database/connection.py
@@ -11,6 +11,7 @@ def get_connection():
             host=os.getenv("DB_HOST"),
             dbname=os.getenv("DB_NAME"),
             user=os.getenv("DB_USER"),
+            port=os.getenv("DB_PORT"),
             password=os.getenv("DB_PASSWORD"),
         )
         return connection
