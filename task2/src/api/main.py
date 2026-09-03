cb2fa08fd1c756dd1b58da87232bff81c1e5bcbe
GajapathiRaoAmpera <gajapathi.mugunda@amperatech.ai>
2026-09-03 17:00:25 +0530

    Added customer_address

--- task2/src/api/main.py
+++ task2/src/api/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from api.routes import customers , customers_group
+from api.routes import customers , customers_group , customer_address
 
 def create_app() -> FastAPI:
     app = FastAPI()
 
-    app.include_router(customers.router)    
+    app.include_router(customers.router)
     app.include_router(customers_group.router)
+    app.include_router(customer_address.router)
 
     @app.get("/")
     def read_root():
